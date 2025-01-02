0ac7d4bd0959dc2380829aeff3320d050bce2069
Muhammad Hammad Aslam <hammadaslam308@gmail.com>
2025-01-03 00:23:30 +0500

    Add react-hook-form and tailwindcss for form handling and styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import questions from "./assets/questions.json";
+import { useForm } from "react-hook-form";
 function App() {
+  const {
+    register,
+    handleSubmit,
+  } = useForm({
+    defaultValues: {
+      answers: [],
+    },
+  });
+  const onSubmit = (data) => {
+    console.log("Form submitted:", data.answers);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <main className="container mx-auto flex flex-col p-4 py-10 gap-8 text-black/80">
+      <h2 className="text-3xl font-bold">Interview QA</h2>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-8">
+        {questions.map((question, index) => (
+          <div
+            key={index}
+            className="bg-gray-100 p-4 rounded-md shadow-md flex flex-col gap-4"
+          >
+            <h3 className="text-2xl font-bold">Question {index + 1}.</h3>
+            <div className="text-xl font-semibold">{question.questionText}</div>
+            <textarea
+              name="answer"
+              id={`answer-${index}`}
+              className="outline-none p-4 resize-none"
+              rows={3}
+              {...register(`answers.${index}.answer`, {
+                required: !question.optional,
+              })}
+            ></textarea>
+          </div>
+        ))}
+        <button
+          type="submit"
+          className="bg-gray-800 hover:bg-gray-900 active:bg-gray-900/90 text-white text-xl transition-all font-semibold p-3 px-6 w-fit rounded-md"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Save
+        </button>
+      </form>
+    </main>
   );
 }
 
